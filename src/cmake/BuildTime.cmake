b6aede5ae0d8cf6c83dede45e24af354d8959c12
Pieter Pas <pieter.pas@outlook.com>
2025-07-23 00:42:25 +0200

    Improve CYQLONE_COMMIT_HASH

--- src/cmake/BuildTime.cmake
+++ src/cmake/BuildTime.cmake
@@ -7,9 +7,18 @@ else()
     execute_process(
         COMMAND git log -1 --format=%H
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-        OUTPUT_VARIABLE BATMAT_COMMIT_HASH
+        OUTPUT_VARIABLE CYQLONE_COMMIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE
         ERROR_QUIET)
+    execute_process(
+        COMMAND git status . --short --no-branch --untracked-files=no
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        OUTPUT_VARIABLE CYQLONE_GIT_STATUS
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET)
+    if (NOT CYQLONE_GIT_STATUS STREQUAL "" AND NOT CYQLONE_COMMIT_HASH STREQUAL "")
+        string(APPEND CYQLONE_COMMIT_HASH "-dirty")
+    endif()
 endif()
 configure_file(${CMAKE_CURRENT_LIST_DIR}/cyqlone-build-time.cpp.in
     cyqlone-build-time.cpp @ONLY)
