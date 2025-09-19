676d0d60cf679cbf3c27872c5e5756ca7c523c6d
Pieter Pas <pieter.pas@outlook.com>
2025-09-19 12:10:58 +0200

    Use release version of Eigen

--- conanfile.py
+++ conanfile.py
@@ -70,9 +70,9 @@ class CyqloneRecipe(ConanFile):
             if self.options.with_conan_python:
                 self.requires("tttapa-python-dev/3.13.7")
         if self.options.with_python or self.options.with_example_problems:
-            self.requires("eigen/tttapa.20250504", transitive_headers=True)
+            self.requires("eigen/3.4.0", transitive_headers=True)
         else:
-            self.test_requires("eigen/tttapa.20250504")
+            self.test_requires("eigen/3.4.0")
         if self.options.get_safe("with_blasfeo"):
             self.requires("blasfeo/0.1.4.1")
         if self.options.get_safe("with_general_qpalm"):
