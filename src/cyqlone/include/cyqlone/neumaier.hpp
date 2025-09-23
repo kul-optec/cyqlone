0dd5b135a846a4bab089ef18aaa688726d0ec4ee
Pieter Pas <pieter.pas@outlook.com>
2025-09-23 23:03:10 +0200

    Neumaier SIMD support

--- src/cyqlone/include/cyqlone/neumaier.hpp
+++ src/cyqlone/include/cyqlone/neumaier.hpp
@@ -7,23 +7,26 @@ namespace cyqlone {
 
 template <class T>
 class NeumaierSum {
-    static_assert(std::is_floating_point_v<T>);
-
-    T sum, compensation = 0;
-    NeumaierSum(T sum, T compensation) : sum(sum), compensation(compensation) {}
-
   public:
-    NeumaierSum(T value = {}) : sum(value), compensation(0) {}
+    T sum, compensation{};
+    NeumaierSum(T sum, T compensation) : sum{sum}, compensation{compensation} {}
+    NeumaierSum(T value = {}) : sum(value) {}
     operator T() const { return sum + compensation; }
 
     NeumaierSum operator-() const { return {-sum, -compensation}; }
     NeumaierSum &operator+=(T v) {
         using std::abs;
         T t = sum + v;
-        if (abs(sum) >= abs(v))
-            compensation += (sum - t) + v;
-        else
-            compensation = (v - t) + sum;
+        if constexpr (std::is_floating_point_v<T>) {
+            if (abs(sum) >= abs(v))
+                compensation += (sum - t) + v;
+            else
+                compensation += (v - t) + sum;
+        } else {
+            auto m = abs(sum) >= abs(v);
+            where(m, compensation) += (sum - t) + v; // TODO: support GSI-HPC/simd
+            where(!m, compensation) += (v - t) + sum;
+        }
         sum = t;
         return *this;
     }
@@ -60,6 +63,19 @@ class NeumaierSum {
     friend NeumaierSum operator-(NeumaierSum lhs, const NeumaierSum &rhs) {
         lhs -= rhs;
         return lhs;
+    }
+    friend NeumaierSum operator*(NeumaierSum lhs, NeumaierSum rhs) {
+        NeumaierSum res = lhs.sum * rhs.sum;
+        res += lhs.sum * rhs.compensation;
+        res += lhs.compensation * rhs.sum;
+        res += lhs.compensation * rhs.compensation;
+        return res;
+    }
+    friend NeumaierSum operator*(NeumaierSum lhs, T rhs) {
+        return {lhs.sum * rhs, lhs.compensation * rhs};
+    }
+    friend NeumaierSum operator*(T lhs, NeumaierSum rhs) {
+        return {rhs.sum * lhs, rhs.compensation * lhs};
     }
 };
 
