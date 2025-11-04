9057d92893424d2607a6f6dd84f98015dae24509
Pieter Pas <pieter.p.dev@outlook.com>
2025-11-04 11:45:45 +0100

    Upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -68,7 +68,7 @@ class CyqloneRecipe(ConanFile):
     generators = ("CMakeDeps",)
 
     def requirements(self):
-        self.requires("guanaqo/1.0.0-alpha.19", transitive_headers=True, transitive_libs=True)
+        self.requires("guanaqo/1.0.0-alpha.20", transitive_headers=True, transitive_libs=True)
         self.requires("batmat/0.0.6", transitive_headers=True, transitive_libs=True)
         if self.options.get_safe("with_python"):
             self.requires("nanobind/2.9.2")
@@ -90,7 +90,7 @@ class CyqloneRecipe(ConanFile):
             self.requires("matio/1.5.27", transitive_headers=True)
         if self.options.get_safe("with_benchmarks"):
             self.requires("benchmark/1.9.4")
-            self.requires("hyhound/1.0.2-alpha.1")
+            self.requires("hyhound/1.1.0")
         self.test_requires("gtest/1.17.0")
 
     def config_options(self):
