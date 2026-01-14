1540b4501c6bb40d569d077c8cc54a3cf9f66186
Pieter Pas <pieter.p.dev@outlook.com>
2026-01-14 01:55:06 +0100

    Add reduction capabilities to barrier

--- src/cyqlone/include/cyqlone/barrier.hpp
+++ src/cyqlone/include/cyqlone/barrier.hpp
@@ -5,7 +5,9 @@
 #include <array>
 #include <atomic>
 #include <cstdint>
+#include <cstring>
 #include <memory>
+#include <type_traits>
 
 #ifndef CYQLONE_SANITY_CHECKS_BARRIER
 #ifndef NDEBUG
@@ -48,6 +50,23 @@ class TreeBarrier {
 
   private:
     static constexpr size_t cache_line_size = 64;
+    struct Storage {
+        alignas(cache_line_size) std::array<std::byte, cache_line_size> payload;
+        template <class T>
+        void store(T t) noexcept {
+            static_assert(sizeof(T) <= sizeof(payload));
+            static_assert(std::is_trivially_copyable_v<T>);
+            std::memcpy(payload.data(), &t, sizeof(T));
+        }
+        template <class T>
+        T load() const noexcept {
+            static_assert(sizeof(T) <= sizeof(payload));
+            static_assert(std::is_trivially_copyable_v<T>);
+            T t;
+            std::memcpy(&t, payload.data(), sizeof(T));
+            return t;
+        }
+    };
     struct alignas(cache_line_size) State {
         using atomic_byte = std::atomic<unsigned char>;
         using atomic_word = std::atomic<uint32_t>;
@@ -61,8 +80,10 @@ class TreeBarrier {
 
     uint32_t expected;
     std::unique_ptr<State[]> state;
+    std::unique_ptr<Storage[]> storage;
+    Storage broadcast;
     [[no_unique_address]] CompletionFn completion;
-    std::atomic<BarrierPhase> phase{};
+    alignas(cache_line_size) std::atomic<BarrierPhase> phase{};
 
 #if CYQLONE_SANITY_CHECKS_BARRIER
     State::ticket_t &get_local_phase(uint32_t thread_id) noexcept {
@@ -95,6 +116,43 @@ class TreeBarrier {
         }
     }
 
+    //                   l=0  l=1  l=2
+    // storage[0]   t=0   1    3    10
+    // storage[1]   t=1   2
+    // storage[2]   t=2   3    7
+    // storage[3]   t=3   4
+
+    template <class T, class F>
+    bool arrive_impl(BarrierPhase old_phase, uint32_t thread_id, T value, F reduce) {
+        static constexpr auto acq_rel = std::memory_order_acq_rel;
+        const auto first_of_one       = static_cast<ticket_value_type>(old_phase),
+                   second_of_two      = static_cast<ticket_value_type>(2 * first_of_one + 1);
+
+        uint32_t level_size = expected; // Total sum in this level of the tree
+        for (size_t level = 0;; ++level) {
+            if (level_size <= 1) {
+                broadcast.store(value);
+                return true;
+            }
+            BATMAT_ASSUME(level < 32);
+            auto offset = size_t{1} << level;
+            auto write  = thread_id << level;
+            storage[write].store(value);
+            thread_id >>= 1;
+            auto &ticket            = state[thread_id].tickets[level];
+            const uint32_t end_node = (level_size + 1) >> 1; // Two threads per node
+            const bool last_odd     = thread_id + 1 == end_node && (level_size & 1) == 1;
+            const auto target       = last_odd ? first_of_one : second_of_two;
+            const auto old_value    = ticket.fetch_add(1, acq_rel);
+            if (old_value != target)
+                return false;
+            if (!last_odd)
+                value = reduce(storage[write & ~offset].template load<T>(),
+                               storage[write | +offset].template load<T>());
+            level_size = end_node;
+        }
+    }
+
   public:
     static constexpr uint32_t max() {
 #if CYQLONE_SANITY_CHECKS_BARRIER // Leave space for local phases for sanity checks
@@ -111,7 +169,12 @@ class TreeBarrier {
         BATMAT_ASSERT(expected <= max());
         const size_t leaf_count = (expected + 1) >> 1;
         state                   = std::make_unique<State[]>(leaf_count);
+        storage                 = std::make_unique<Storage[]>(expected);
     }
+    TreeBarrier(const TreeBarrier &)            = delete;
+    TreeBarrier(TreeBarrier &&)                 = default;
+    TreeBarrier &operator=(const TreeBarrier &) = delete;
+    TreeBarrier &operator=(TreeBarrier &&)      = default;
 
     [[nodiscard]] arrival_token arrive(uint32_t thread_id) {
         BATMAT_ASSUME(thread_id < expected);
@@ -162,6 +225,17 @@ class TreeBarrier {
 
     uint32_t spin_count = 1000; // approx. 2-3 cycles/iteration (Haswell, according to llvm-mca)
 
+    /// Check if @ref wait() may block. If it returns false, the caller can call @ref wait()
+    /// and it will return immediately without spinning or sleeping. This is useful if the caller
+    /// has other non-critical work to do while waiting for other threads.
+    /// Users should still call @ref wait() before arriving again.
+    /// @note This function does not impose any memory ordering, so even when it returns false,
+    ///       changes made before the arrival of other threads may not be visible yet. In contrast,
+    ///       @ref wait() does ensure proper synchronization.
+    bool wait_may_block(const arrival_token &token) const noexcept {
+        return phase.load(std::memory_order_relaxed) == token.get();
+    }
+
     void wait(arrival_token &&token) const {
         const auto old_phase = token.get();
         // barring overflow, we have that current_phase >= old_phase
@@ -176,6 +250,21 @@ class TreeBarrier {
 
     void arrive_and_wait(uint32_t thread_id) { wait(arrive(thread_id)); }
     void arrive_and_wait(uint32_t thread_id, int line) { wait(arrive(thread_id, line)); }
+
+    /// Combining tree reduction across all threads. Deterministic for a given number of threads.
+    template <class T, class F>
+    [[nodiscard]] T reduce(uint32_t thread_id, T x, F reduce) {
+        BATMAT_ASSUME(thread_id < expected);
+        const auto cur_phase = phase.load(std::memory_order_relaxed);
+        if (arrive_impl(cur_phase, thread_id, std::move(x), std::move(reduce))) {
+            completion();
+            auto next_phase = static_cast<BarrierPhase>(static_cast<PhaseType>(cur_phase) + 1);
+            phase.store(next_phase, std::memory_order_release);
+            phase.notify_all();
+        }
+        wait(arrival_token{cur_phase});
+        return broadcast.template load<T>();
+    }
 };
 
 } // namespace cyqlone
--- test/test-barrier.cpp
+++ test/test-barrier.cpp
@@ -29,7 +29,7 @@ TEST_P(BarrierTest, stress) {
                 success += decltype(barrier)::BarrierPhase{iter} == barrier.current_phase();
                 barrier.arrive_and_wait(i + 1); // worker IDs start at 1
             }
-            EXPECT_EQ(success, 2 * iterations);
+            EXPECT_EQ(success, 2 * iterations) << i;
         });
     }
 
@@ -59,14 +59,14 @@ TEST_P(BarrierTest, std) {
 
     // Worker threads
     for (uint32_t i = 0; i < num_workers; ++i) {
-        threads.emplace_back([&] {
+        threads.emplace_back([&, i] {
             uint32_t success = 0;
             for (uint32_t iter = 0; iter < iterations; ++iter) {
                 uint32_t val = counter.load(std::memory_order_relaxed);
                 success += (val == iter || val == iter + 1);
                 barrier.arrive_and_wait();
             }
-            EXPECT_EQ(success, iterations);
+            EXPECT_EQ(success, iterations) << i;
         });
     }
 
@@ -80,6 +80,29 @@ TEST_P(BarrierTest, std) {
 }
 #endif
 
+TEST_P(BarrierTest, reduce) {
+    const uint32_t num_workers = GetParam();
+    const uint32_t iterations  = num_workers < 10 ? 1'000'000 : 1'000;
+
+    using Barrier = cyqlone::TreeBarrier<>;
+    ASSERT_GE(Barrier::max(), num_workers);
+    Barrier barrier(num_workers, {});
+
+    std::vector<std::jthread> threads;
+    threads.reserve(num_workers);
+
+    for (uint32_t i = 0; i < num_workers; ++i) {
+        threads.emplace_back([&, i] {
+            uint32_t success = 0;
+            for (uint32_t iter = 0; iter < iterations; ++iter) {
+                auto res = barrier.reduce(i, i + 1, std::plus{});
+                success += res == num_workers * (num_workers + 1) / 2;
+            }
+            EXPECT_EQ(success, iterations) << i;
+        });
+    }
+}
+
 // Instantiate the test with different numbers of worker threads
 INSTANTIATE_TEST_SUITE_P(BarrierTest, BarrierTest,
-                         ::testing::Values(1, 2, 3, 4, 5, 6, 7, 254, 255, 256, 257));
+                         ::testing::Values(1, 2, 3, 4, 5, 6, 7, 8, 9, 254, 255, 256, 257));
