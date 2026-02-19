094d3747af65941c14cc9477dabd3dfe6762c97d
Pieter Pas <pieter.p.dev@outlook.com>
2026-02-19 14:01:56 +0100

    [Test] add transposed packing/unpacking tests

--- test/test-linalg.cpp
+++ test/test-linalg.cpp
@@ -75,6 +75,46 @@ TEST(LinAlg, unpackGeneral) {
     }
 }
 
+TEST(LinAlg, unpackGeneralTransB) {
+    using cyqlone::index_t;
+    using vl_t  = std::integral_constant<index_t, 4>;
+    using one_t = std::integral_constant<index_t, 1>;
+    batmat::matrix::Matrix<double, index_t, vl_t, vl_t> A{{.rows = 13, .cols = 17}};
+    batmat::matrix::Matrix<double, index_t, one_t, vl_t> B{{.rows = 17, .cols = 13}};
+    fill_linear(A, 0.5, 0.25);
+
+    cyqlone::linalg::unpack(A, B.transposed());
+    for (index_t l = 0; l < A.depth(); ++l) {
+        for (index_t r = 0; r < A.rows(); ++r) {
+            for (index_t c = 0; c < A.cols(); ++c) {
+                const double expected = A(l, r, c);
+                const double actual   = B(l, c, r); // Transposed access
+                EXPECT_DOUBLE_EQ(actual, expected) << " at (" << l << ", " << r << ", " << c << ")";
+            }
+        }
+    }
+}
+
+TEST(LinAlg, unpackGeneralTransA) {
+    using cyqlone::index_t;
+    using vl_t  = std::integral_constant<index_t, 4>;
+    using one_t = std::integral_constant<index_t, 1>;
+    batmat::matrix::Matrix<double, index_t, vl_t, vl_t> A{{.rows = 17, .cols = 13}};
+    batmat::matrix::Matrix<double, index_t, one_t, vl_t> B{{.rows = 13, .cols = 17}};
+    fill_linear(A, 0.5, 0.25);
+
+    cyqlone::linalg::unpack(A.transposed(), B);
+    for (index_t l = 0; l < A.depth(); ++l) {
+        for (index_t r = 0; r < A.rows(); ++r) {
+            for (index_t c = 0; c < A.cols(); ++c) {
+                const double expected = A(l, r, c);
+                const double actual   = B(l, c, r); // Transposed access
+                EXPECT_DOUBLE_EQ(actual, expected) << " at (" << l << ", " << r << ", " << c << ")";
+            }
+        }
+    }
+}
+
 TEST(LinAlg, packGeneral) {
     using cyqlone::index_t;
     using vl_t  = std::integral_constant<index_t, 4>;
@@ -88,6 +128,46 @@ TEST(LinAlg, packGeneral) {
         for (index_t r = 0; r < A.rows(); ++r) {
             for (index_t c = 0; c < A.cols(); ++c) {
                 const double expected = B(l, r, c);
+                const double actual   = A(l, r, c);
+                EXPECT_DOUBLE_EQ(actual, expected) << " at (" << l << ", " << r << ", " << c << ")";
+            }
+        }
+    }
+}
+
+TEST(LinAlg, packGeneralTransB) {
+    using cyqlone::index_t;
+    using vl_t  = std::integral_constant<index_t, 4>;
+    using one_t = std::integral_constant<index_t, 1>;
+    batmat::matrix::Matrix<double, index_t, vl_t, vl_t> A{{.rows = 3, .cols = 5}};
+    batmat::matrix::Matrix<double, index_t, one_t, vl_t> B{{.rows = 5, .cols = 3}};
+    fill_linear(B, 0.5, 0.25);
+
+    cyqlone::linalg::pack(B.transposed(), A);
+    for (index_t l = 0; l < A.depth(); ++l) {
+        for (index_t r = 0; r < A.rows(); ++r) {
+            for (index_t c = 0; c < A.cols(); ++c) {
+                const double expected = B(l, c, r); // Transposed access
+                const double actual   = A(l, r, c);
+                EXPECT_DOUBLE_EQ(actual, expected) << " at (" << l << ", " << r << ", " << c << ")";
+            }
+        }
+    }
+}
+
+TEST(LinAlg, packGeneralTransA) {
+    using cyqlone::index_t;
+    using vl_t  = std::integral_constant<index_t, 4>;
+    using one_t = std::integral_constant<index_t, 1>;
+    batmat::matrix::Matrix<double, index_t, vl_t, vl_t> A{{.rows = 5, .cols = 3}};
+    batmat::matrix::Matrix<double, index_t, one_t, vl_t> B{{.rows = 3, .cols = 5}};
+    fill_linear(B, 0.5, 0.25);
+
+    cyqlone::linalg::pack(B, A.transposed());
+    for (index_t l = 0; l < A.depth(); ++l) {
+        for (index_t r = 0; r < A.rows(); ++r) {
+            for (index_t c = 0; c < A.cols(); ++c) {
+                const double expected = B(l, c, r); // Transposed access
                 const double actual   = A(l, r, c);
                 EXPECT_DOUBLE_EQ(actual, expected) << " at (" << l << ", " << r << ", " << c << ")";
             }
