b277cbb85981e5142bfe016e4bdb224b20895f11
Pieter Pas <pieter.p.dev@outlook.com>
2026-03-03 10:47:55 +0100

    Upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -73,8 +73,8 @@ class CyqloneRecipe(ConanFile):
     generators = ("CMakeDeps",)
 
     def requirements(self):
-        self.requires("guanaqo/1.0.0-alpha.24", transitive_headers=True, transitive_libs=True)
-        self.requires("batmat/0.0.16", transitive_headers=True, transitive_libs=True)
+        self.requires("guanaqo/1.0.0-alpha.25", transitive_headers=True, transitive_libs=True)
+        self.requires("batmat/0.0.17", transitive_headers=True, transitive_libs=True)
         if self.options.get_safe("with_zlib"):
             self.requires("zlib/[~1]")
         if self.options.get_safe("with_python"):
