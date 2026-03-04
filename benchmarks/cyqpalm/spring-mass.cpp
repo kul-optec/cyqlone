d37813630eac288a4c7737e02e58ce88fdef07de
Pieter Pas <pieter.p.dev@outlook.com>
2026-03-04 16:54:12 +0100

    Add more metadata to benchmark

--- benchmarks/cyqpalm/spring-mass.cpp
+++ benchmarks/cyqpalm/spring-mass.cpp
@@ -14,9 +14,11 @@
 #include <guanaqo/pcm/counters.hpp>
 #include <guanaqo/perfetto/trace.hpp>
 #include <guanaqo/string-util.hpp>
+#include <guanaqo/stringify.h>
 #include <batmat-version.h>
 #include <cyqlone-version.h>
 #include <algorithm>
+#include <cstdlib>
 #include <filesystem>
 #include <format>
 #include <generator>
@@ -534,7 +536,9 @@ int initialize_google_benchmark(char *program, auto bm_args) {
 
 void register_context() {
 #if BATMAT_WITH_OPENMP
+    auto places = std::getenv("OMP_PLACES");
     benchmark::AddCustomContext("OMP_NUM_THREADS", std::to_string(omp_get_max_threads()));
+    benchmark::AddCustomContext("OMP_PLACES", places ? places : "");
 #endif
     benchmark::AddCustomContext("batmat_build_time", batmat_build_time);
     benchmark::AddCustomContext("batmat_commit_hash", batmat_commit_hash);
@@ -542,12 +546,16 @@ void register_context() {
     benchmark::AddCustomContext("cyqlone_commit_hash", cyqlone_commit_hash);
 #if defined(__INTEL_LLVM_COMPILER)
     benchmark::AddCustomContext("compiler", "intel-llvm");
+    benchmark::AddCustomContext("compiler_version", __VERSION__);
 #elif defined(__clang__)
     benchmark::AddCustomContext("compiler", "clang");
+    benchmark::AddCustomContext("compiler_version", __VERSION__);
 #elif defined(__GNUC__)
     benchmark::AddCustomContext("compiler", "gcc");
+    benchmark::AddCustomContext("compiler_version", __VERSION__);
 #elif defined(_MSC_VER)
     benchmark::AddCustomContext("compiler", "msvc");
+    benchmark::AddCustomContext("compiler_version", GUANAQO_STRINGIFY(_MSC_FULL_VER));
 #endif
 #if defined(__AVX512F__)
     benchmark::AddCustomContext("arch", "avx512f");
@@ -559,6 +567,11 @@ void register_context() {
     benchmark::AddCustomContext("arch", "sse3");
 #elif defined(__ARM_NEON)
     benchmark::AddCustomContext("arch", "neon");
+#endif
+#if BATMAT_WITH_GSI_HPC_SIMD
+    benchmark::AddCustomContext("simd_library", "GSI-HPC/simd");
+#else
+    benchmark::AddCustomContext("simd_library", "libstdc++ <experimental/simd>");
 #endif
 }
 
